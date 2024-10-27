28a12dd2c7bd1a4100c03e1a37090df94fdd57e6
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-10-27 17:59:53 +0530

    added movie page

---
+++ src/app/movie/[id]/page.jsx
@@ -0,0 +1,36 @@
+import { Span } from 'next/dist/trace';
+import Image from 'next/image';
+import React from 'react';
+
+const MoviePage = async ({params}) => {
+    const movieData = await params;
+    const movieId = movieData.id;
+    const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+    const res = await fetch(url);
+
+    const movie = await res.json();
+  return (
+    <div className='w-full'>
+        <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
+            <Image
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path || movie.backdrop_path}`}
+            height={500} width={300} alt="" className="rounded-lg"
+            style={{maxWidth:"100%", height: "100%"}}
+            >
+            </Image>
+            <div className="p-2">
+                <h2 className="text-lg mb-3 font-bold">{movie.title || movie.name}</h2>
+                <p className="text-lg mb-3">{movie.overview}</p>
+                <p className="mb-3">
+                    <span className="font-semibold mr-1">Date Released:</span>
+                    {movie.release_date || movie.first_air_date}</p>
+                <p className="mb-3">
+                <span className="font-semibold mr-1">Rating:</span>
+                    {movie.vote_count}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MoviePage
